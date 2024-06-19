aacb3ba4dd7cfa52ba8c0d068f2330d4878b8f62
lenakarnaukh <game.bit.lena@gmail.com>
2024-06-19 22:27:28 +0300

    Fixed import in navigation

--- src/components/common/Header/Navigation.tsx
+++ src/components/common/Header/Navigation.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import '@/components/common/Header/Navigation.css'
+
 export function Navigation() {
     const pathName = usePathname()
     return (
